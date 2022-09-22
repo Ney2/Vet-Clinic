ebdafee64ce415a63baae1de147f3285faab4878
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-09-22 11:27:41 +0300

    Add join tables

--- schema.sql
+++ schema.sql
@@ -30,3 +30,31 @@ CREATE TABLE species (
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals Add COLUMN owner_id INT REFERENCES owners(id);
+
+/* Create vets table */
+CREATE TABLE vets (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    date_of_graduation DATE NOT NULL,
+    PRIMARY KEY(id)
+);
+
+/* Create join table named specializations */
+CREATE TABLE specializations (
+    species_id INT,
+    vet_id INT,
+    PRIMARY KEY(species_id, vet_id),
+    FOREIGN KEY(species_id) REFERENCES species(id),
+    FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
+
+/* Create join table named visits */
+CREATE TABLE visits (
+    animal_id INT,
+    vet_id INT,
+    visit_date DATE,
+    PRIMARY KEY(animal_id, vet_id, visit_date),
+    FOREIGN KEY(animal_id) REFERENCES animals(id),
+    FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
